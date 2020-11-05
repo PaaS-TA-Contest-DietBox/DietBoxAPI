03800f297dbd404e5561fc3eaeca7a8ae163afdf
minuk8932 <minuk8932@naver.com>
2020-11-05 13:53:14 +0900

    add Model & Enums

---
+++ src/main/java/com/example/paasta/DietBoxAPI/models/data_enums/RDA.java
@@ -0,0 +1,97 @@
+package com.example.paasta.DietBoxAPI.models.data_enums;
+
+public enum RDA {
+    RECOMMENDED_DAILY_ALLOWANCE(2600, 390
+            , 130, 86, 78
+            , 1.5, 0.3
+            , 20, 3);
+
+    private double kcal;
+    private double carbohydrate, protein, fat;
+    private double sugar, sodium;
+    private double cholesterol, saturatedFattyAcid, transFat;
+
+    RDA(double kcal, double carbohydrate, double protein, double fat, double sugar, double sodium, double cholesterol, double saturatedFattyAcid, double transFat) {
+        this.kcal = kcal;
+        this.carbohydrate = carbohydrate;
+        this.protein = protein;
+        this.fat = fat;
+        this.sugar = sugar;
+        this.sodium = sodium;
+        this.cholesterol = cholesterol;
+        this.saturatedFattyAcid = saturatedFattyAcid;
+        this.transFat = transFat;
+    }
+
+    public double getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(double kcal) {
+        this.kcal = kcal;
+    }
+
+    public double getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public void setCarbohydrate(double carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+
+    public double getSodium() {
+        return sodium;
+    }
+
+    public void setSodium(double sodium) {
+        this.sodium = sodium;
+    }
+
+    public double getCholesterol() {
+        return cholesterol;
+    }
+
+    public void setCholesterol(double cholesterol) {
+        this.cholesterol = cholesterol;
+    }
+
+    public double getSaturatedFattyAcid() {
+        return saturatedFattyAcid;
+    }
+
+    public void setSaturatedFattyAcid(double saturatedFattyAcid) {
+        this.saturatedFattyAcid = saturatedFattyAcid;
+    }
+
+    public double getTransFat() {
+        return transFat;
+    }
+
+    public void setTransFat(double transFat) {
+        this.transFat = transFat;
+    }
+}
