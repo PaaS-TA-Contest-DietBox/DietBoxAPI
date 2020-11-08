3e28ef7ab5152ac82de559c7c7e8f5c65bdde721
minuk8932 <minuk8932@naver.com>
2020-11-08 13:12:20 +0900

    Deployment completion with paas-ta

--- src/main/java/com/example/paasta/DietBoxAPI/DietBoxApiApplication.java
+++ src/main/java/com/example/paasta/DietBoxAPI/DietBoxApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DietBoxApiApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "paasta/DietBoxAPI");
 		SpringApplication.run(DietBoxApiApplication.class, args);
 	}
 
