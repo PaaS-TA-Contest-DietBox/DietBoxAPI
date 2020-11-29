7f6c96ce3978b06bb4a36507690c13067b08993d
minuk8932 <minuk8932@naver.com>
2020-11-29 18:49:28 +0900

    Push completion, handled mysql connector error by downgrade

--- src/main/java/com/example/DietBoxAPI/ServletInitializer.java
+++ src/main/java/com/example/DietBoxAPI/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DietBoxApiApplication.class).profiles("spring.config.name: DietBoxAPI");
+		return application.sources(DietBoxApiApplication.class);
 	}
 
 }
--- src/main/java/com/example/DietBoxAPI/configurations/CloudConfigData.java
+++ src/main/java/com/example/DietBoxAPI/configurations/CloudConfigData.java
@@ -3,36 +3,41 @@ package com.example.DietBoxAPI.configurations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.Cloud;
 import org.springframework.cloud.CloudFactory;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.config.java.ServiceScan;
 import org.springframework.cloud.service.common.MysqlServiceInfo;
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("classpath:application-cloud.properties")
-public class CloudConfigData {
-    @Value("${db.mysql.servicename}")
-    private String mysqlServiceName;
+@Profile("cloud")
+@ServiceScan
+@Component
+public class CloudConfigData extends AbstractCloudConfig {
+	@Value("${db.mysql.servicename}")
+	private String mysqlServiceName;
 
-    @Bean(name = "dsMysql")
-    @Primary
-    DataSource mysqlDataSource() {
+	@Bean(name = "dsMysql")
+	@Primary
+	DataSource mysqlDataSource() {
 
-        CloudFactory cloudFactory = new CloudFactory();
-        Cloud cloud = cloudFactory.getCloud();
-        MysqlServiceInfo serviceInfo = (MysqlServiceInfo) cloud.getServiceInfo(mysqlServiceName);
-        String serviceId = serviceInfo.getId();
-        return cloud.getServiceConnector(serviceId, DataSource.class, null);
+		CloudFactory cloudFactory = new CloudFactory();
+		Cloud cloud = cloudFactory.getCloud();
+		MysqlServiceInfo serviceInfo = (MysqlServiceInfo) cloud.getServiceInfo(mysqlServiceName);
+		String serviceId = serviceInfo.getId();
+		return cloud.getServiceConnector(serviceId, DataSource.class, null);
 
-    }
+	}
 
-    @Bean(name = "jdbcMysql")
-    @Autowired
-    public JdbcTemplate mysqlJdbcTemplate(@Qualifier("dsMysql") DataSource dsSlave) {
-        return new JdbcTemplate(dsSlave);
-    }
+	@Bean(name = "jdbcMysql")
+	@Autowired
+	public JdbcTemplate mysqlJdbcTemplate(@Qualifier("dsMysql") DataSource dsSlave) {
+		return new JdbcTemplate(dsSlave);
+	}
 }
